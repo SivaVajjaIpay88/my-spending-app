4074be2e392026e7f35aec3310478f695881e0c0
Siva Teja Vajja <sivavajja@ipay88.com.my>
2019-05-26 10:23:11 +0800

    day 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,101 +1,77 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  Platform,
+  Button
 } from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+class Row extends React.Component {
+  render() {
+    const { desc, price, onPress } = this.props;
+    return (
+      <View style={styles.cardRow}>
+        <Text>{desc}</Text>
+        <Text>RM {price}</Text>
+        <Button title="Button" onPress={() => onPress(desc)} />
+      </View>
+    )
+  }
+}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title: 'Spending',
   };
 
   render() {
+    const date = new Date();
+    const data = [{ desc: "food", price: 1, onPress: (x) => alert(x) }, { desc: "food", price: 2 }];
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+          <View style={styles.card}>
+            <View style={styles.cardDate}>
+              <Text>{date.toLocaleDateString()}</Text>
+            </View>
+            {data.map((item, index) => {
+              return (
+                <Row
+                  key={index}
+                  desc={item.desc}
+                  price={item.price}
+                  onPress={item.onPress}
+                />
+              )
+            })
+            }
 
-            <Text style={styles.getStartedText}>Hello World welcome to my Home Screen</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+            <View style={styles.cardRow}>
+              <Text>Food</Text>
+              <Text>RM 8</Text>
+            </View>
+            <View style={styles.cardRow}>
+              <Text>Grocery</Text>
+              <Text>RM 2</Text>
             </View>
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
+            <View style={styles.cardRow}>
+              <Text>Travel</Text>
+              <Text>RM 20</Text>
+            </View>
           </View>
         </ScrollView>
 
         <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>hello world:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+          <Text style={{ fontWeight: 'bold' }}>Total</Text>
+          <Text style={{ fontWeight: 'bold' }}>RM 30</Text>
         </View>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -103,48 +79,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+    // paddingTop: 30,
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -165,24 +101,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fbfbfb',
     paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
   },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+  card: {
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
   },
-  navigationFilename: {
-    marginTop: 5,
+  cardDate: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    backgroundColor: '#f5f5f5'
   },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  cardRow: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
   },
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,19 +1,76 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  Platform,
+  View,
+  TouchableOpacity,
+  DatePickerIOS,
+  DatePickerAndroid
+} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Add Item',
   };
-
+  state = { amount: '', desc: '', date: new Date() }
   render() {
+
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView style={styles.container}>
+
+          <View style={styles.viewRowDir1} >
+            <Text style={{ fontSize: 18 }}>Price</Text>
+            <TextInput
+              style={styles.textInput1}
+              onChangeText={(input) => this.setState({ amount: input })}
+              value={this.state.text}
+              keyboardType='numeric'
+              placeholder="RM"
+            />
+          </View>
+
+          <View style={styles.viewRowDir2} >
+            <TextInput
+              style={styles.textInput2}
+              onChangeText={(desc) => this.setState({ desc })}
+              value={this.state.text}
+              keyboardType='default'
+              placeholder="Description"
+            />
+          </View>
+
+          <View style={styles.cardDate}>
+            <View style={{ flex: 1 }}>
+              {
+                Platform.OS === 'ios' ?
+                  <DatePickerIOS
+                    date={this.state.date}
+                    onDateChange={(date) => this.setState({ date })}
+                  /> :
+                  <TouchableOpacity
+                    onPress={async () => {
+                      const { year, month, day } = await DatePickerAndroid.open({
+                        date: new Date()
+                      });
+                      this.setState({ date: new Date(year, month, day) })
+                    }}>
+                    <Text>{this.state.date.toString()}</Text>
+                  </TouchableOpacity>
+              }
+            </View>
+          </View>
+
+        </ScrollView>
+        <TouchableOpacity onPress={() => alert(JSON.stringify(this.state))} style={styles.tabBarInfoContainer}>
+          <Text
+            style={styles.ADDButtonStyle}
+            fontWeight='bold'>ADD</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -21,7 +78,81 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
   },
+  cardRow: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: 'red',
+  },
+  viewRowDir1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 10,
+  },
+  viewRowDir2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 10,
+  },
+  textInput1: {
+    width: 50,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 5,
+    color: 'white',
+    backgroundColor: 'grey',
+  },
+  textInput2: {
+    width: "100%",
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    padding: 5,
+    color: 'white',
+    backgroundColor: 'grey',
+  },
+  cardDate: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    backgroundColor: '#f5f5f5',
+    alignItems:'center'
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: 'red',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    color: 'white',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  ADDButtonStyle: {
+    width: "100%",
+  }
 });
